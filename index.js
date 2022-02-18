58714974cbbb334e0a77eb53f38dfbcbe9ec7303
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-02-18 03:56:27 -0500

    here is some boiler plate

--- index.js
+++ index.js
@@ -10,36 +10,11 @@ function myFunction() {
   }
 
 //This is for the Book Genres
-
+/* 
 let bookGenres = () => {
   for( i=0; i < array.length; i++)
     if(array[i] == 0) {
         array[i] = newValue;
         break;
     }
-}   
-
-//Example array sort
-
-const numbers = [75, 8, 34, 26, 4, 0, 52] ;
-
-numbers.sort(compareNumbers) ;
-
-console.log(numbers) ;
-
-function compareNumbers (a, b) {
-  return a - b; 
-}
-
-
-//Adds filter array
-
-//Adds Books to the list
-
-//Adds permission prompt for deletion
-
-//Adds delete books with permission
-
-//Adds hide books from list
-
-//This JavaScript is specifice to the book search query.
+}  */ 
