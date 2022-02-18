c7407ccd97856853d73d5fc2910fd7842dc0c2a8
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-02-18 14:32:06 -0500

    index is not being seen in folder.

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-//this for the topnavigation. Uses the icon and responsive menu for all devices.
-
+//Responsive top-navigation function
 function myFunction() {
-    let x = document.getElementById("Topnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-
-//This is for the Book Genres
-/* 
-let bookGenres = () => {
-  for( i=0; i < array.length; i++)
-    if(array[i] == 0) {
-        array[i] = newValue;
-        break;
-    }
-}  */ 
+} 
