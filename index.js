0e9f5dacc4fe18dffee33ab0f51ba4e98ecdada5
Margaret Farmer <86688818+Marvista8@users.noreply.github.com>
2022-02-19 09:49:37 -0500

    well here goes nothing.

--- index.js
+++ index.js
@@ -15,3 +15,40 @@ searchBar.addEventListener('keypu', (event) => {
 })
 
 const namesFromDOM = document.getElementsByClassName("Title");
+
+
+function validateForm() {
+  let x = document.forms["bookform"]["Title"].value;
+  if (x == "") {
+  alert("Ttile must be filled out");
+  return false;
+  }
+}
+
+      // Form validation code will come here.
+      function validate() {
+      
+         if( document.myForm.Name.value == "" ) {
+            alert( "Please provide your name!" );
+            document.myForm.Name.focus() ;
+            return false;
+         }
+         if( document.myForm.EMail.value == "" ) {
+            alert( "Please provide your Email!" );
+            document.myForm.EMail.focus() ;
+            return false;
+         }
+         if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
+            document.myForm.Zip.value.length != 5 ) {
+            
+            alert( "Please provide a zip in the format #####." );
+            document.myForm.Zip.focus() ;
+            return false;
+         }
+         if( document.myForm.Country.value == "-1" ) {
+            alert( "Please provide your country!" );
+            return false;
+         }
+         return( true );
+      }
+   //-->
